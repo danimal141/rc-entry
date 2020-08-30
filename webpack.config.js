b2dc9e2c6eec5ed1da6e0ddddfb69fa664364ca6
danimal141 <hideaki.ishii1204@gmail.com>
2020-08-30 14:23:42 +0900

    Configure webpack to build ts

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+      bundle: './src/index.ts'
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].js'
+    },
+    resolve: {
+      extensions: ['.ts','.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,loader:'ts-loader'
+        }
+      ]
+    }
+}
