8e06489aa7d413ddb1e952c4a1f42c1f9d00c8b5
danimal141 <hideaki.ishii1204@gmail.com>
2022-01-05 16:18:03 +0900

    Fix class

--- src/entry.ts
+++ src/entry.ts
@@ -2,7 +2,7 @@ import { Cookie, Page } from 'puppeteer'
 
 const ENTRY_URL = 'https://event.rakuten.co.jp/campaign/card/pointday/'
 
-export const TARGET_BUTTON_CLASS = '.riCheckEntryMulti__noLoginButton'
+export const TARGET_BUTTON_CLASS = '.rcEntryButton-button'
 
 export default async function entry(page: Page, cookies: Cookie[]) {
   for (const cookie of cookies) { await page.setCookie(cookie) }
@@ -10,8 +10,8 @@ export default async function entry(page: Page, cookies: Cookie[]) {
   await page.goto(ENTRY_URL, { timeout: 30000, waitUntil: 'domcontentloaded' })
 
   if (await page.$(TARGET_BUTTON_CLASS) == null) {
-    console.error('There seems to be no campaign held now')
-    process.exit(1)
+    console.log('There seems to be no campaign held now')
+    process.exit(0)
   }
   await page.click(TARGET_BUTTON_CLASS)
   await page.waitForNavigation({ timeout: 30000, waitUntil: 'domcontentloaded' })
